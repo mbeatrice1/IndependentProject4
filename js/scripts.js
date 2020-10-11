d9c104e2c73b72d696da1e58b745e7816729716e
Beatrice <mahorob7@gmail.com>
2020-10-11 23:27:48 +0200

    added some features

--- js/scripts.js
+++ js/scripts.js
@@ -19,16 +19,16 @@ function getCrust() {
     return parseInt(c);
 }
 function getDelivery() {
-    //var d = document.getElementById("dv").value;
-    //return d;
-    alert("The delivery  charge of 2000 will be added to your bill");
+    var d = document.getElementById("dv").value;
+    return d;
+    //alert("The delivery  charge of 2000 will be added to your bill");
 }
 function calctotalPrice(e) {
-    //.preventDefault();
+    //preventDefault();
     //var totalPrice = (getCrust() + getToppping());
     
-        //console.log(totalPrice);
-   // alert("Your order of " + si + " is completed.Your total amount you are requested to pay" + totalPrice +"." )//
+       // console.log(totalPrice);
+   alert("Hello Your order of  is completed.Your total amount you are requested to pay is." )//
 alert("Thank you")
 }
 
