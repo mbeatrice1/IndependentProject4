5c0d8bfb42c5809599952a789ed1ec28882a9912
Beatrice <mahorob7@gmail.com>
2020-10-11 22:23:59 +0200

    added Js content

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+ 
+function getName() {
+    var nam = document.getElementById("na").value;
+    return parseInt(nam);
+}
+
+function getSize() {
+    var si = document.getElementById("pizza").value;
+    return parseInt(si);
+}
+
+function getToppping(){
+    var top = document.getElementById("tp").value;
+    return parseInt(top);
+}
+
+function getCrust() {
+    var c = document.getElementById("cs").value;
+    return parseInt(c);
+}
+function getDelivry() {
+    var d = document.getElementById("dv").value;
+    return parseInt(d);
+}
